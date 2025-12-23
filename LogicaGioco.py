df2d3eed25c1cd0ff3765f8a1269190ed2ad997a
itsgiuliamaltese <itsgiuliamaltese@gmail.com>
2025-12-23 17:19:50 +0100

    commento

--- LogicaGioco.py
+++ LogicaGioco.py
@@ -5,7 +5,7 @@ from typing import List, Dict, Any
 # ==========================================
 # 1. PATTERN OBSERVER & MEMENTO (NUOVI)
 # ==========================================
-#provaprova
+
 class Observer(ABC):
     @abstractmethod
     def update(self, subject: Subject) -> None:
